55537fbfdc33d4932b8e5edabff41e8d57e92818
winwarphoo <winwarphoo@mm.cybermissions.co.jp>
2022-02-14 16:45:56 +0900

    SignUp画面の追加

---
+++ src/components/SignUp.js
@@ -0,0 +1,26 @@
+const  SignUp = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("登録")
+  }
+  return (
+    <div>
+      <h1>ユーザ登録</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>メールアドレス</label>
+          <input name="email" type="email" placeholder="email" />
+        </div>
+        <div>
+          <label>パスワード</label>
+          <input name="password" type="password" />
+        </div>
+        <div>
+          <button>登録</button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default SignUp;
