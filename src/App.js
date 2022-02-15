c619ea31f2abc76b964b58b849bbd42313efdf4a
winwarphoo <winwarphoo@mm.cybermissions.co.jp>
2022-02-15 15:44:04 +0900

    add Auth

--- src/App.js
+++ src/App.js
@@ -3,18 +3,21 @@ import SignUp from './components/SignUp';
 import Home from './components/Home';
 import Login from './components/Login';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
 
 function App() {
   return (
-    <div className='App'>
-      <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/signup" element={<SignUp />}></Route>
-        <Route path="/login" element={<Login />}></Route>
-      </Routes>
-      </BrowserRouter>
-    </div>
+    <AuthProvider>
+      <div className='App'>
+        <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/signup" element={<SignUp />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+        </Routes>
+        </BrowserRouter>
+      </div>
+    </AuthProvider>
   );
 }
 
---
+++ src/context/AuthContext.js
@@ -0,0 +1,24 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { auth } from '../firebase';
+
+const AuthContext = createContext();
+
+export function useAuthContext() {
+  return useContext(AuthContext);
+}
+
+export function AuthProvider({ children }) {
+  const [user, setUser] = useState("");
+  const value = {
+    user,
+  };
+
+  useEffect(() => {
+    const unsubscribed = auth.onAuthStateChanged((user) => {
+      setUser(user);
+    });
+    return () => unsubscribed();
+  }, []);
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+}
